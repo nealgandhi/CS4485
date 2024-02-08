ecbad56da925da7433dd61af7b9d552a21180d7d
YaraJan <102333657+YaraJan@users.noreply.github.com>
2024-02-07 21:25:50 -0600

    Login form: removed google sign in button and fixed formatting

--- frontend/schedule-planner/src/components/LoginForm.js
+++ frontend/schedule-planner/src/components/LoginForm.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 function LoginForm() {
     return (
-        <div className='w-1/2 bg-white px-10 py-5 rounded-3xl border-2 border-blue-800'>
+        <div className='w-3/4 lg:w-1/2 bg-white px-10 py-5 rounded-3xl border-2 border-blue-800'>
             <h1 className='text-5xl font-semibold'>Login</h1>
             <p className='font-medium text-lg text-gray-500 mt-4'>Welcome back! Please enter your details.</p>
-            <div className='mt-6'>
+            <div className='mt-8'>
                 <div className='flex flex-col'>
                     <label className='text-lg font-medium'>Email</label>
                     <input
@@ -33,7 +33,7 @@ function LoginForm() {
                 </div>
                 <div className='mt-6 flex flex-col gap-y-4'>
                     <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-blue-800 text-white text-lg font-bold'>Sign in</button>
-                    <button className='flex py-2 rounded-xl border-2 border-blue-100 items-center justify-center gap-2 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all'>
+                    {/* <button className='flex py-2 rounded-xl border-2 border-blue-100 items-center justify-center gap-2 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all'>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M5.26644 9.76453C6.19903 6.93863 8.85469 4.90909 12.0002 4.90909C13.6912 4.90909 15.2184 5.50909 16.4184 6.49091L19.9093 3C17.7821 1.14545 15.0548 0 12.0002 0C7.27031 0 3.19799 2.6983 1.24023 6.65002L5.26644 9.76453Z" fill="#EA4335"/>
                             <path d="M16.0406 18.0142C14.9508 18.718 13.5659 19.0926 11.9998 19.0926C8.86633 19.0926 6.21896 17.0785 5.27682 14.2695L1.2373 17.3366C3.19263 21.2953 7.26484 24.0017 11.9998 24.0017C14.9327 24.0017 17.7352 22.959 19.834 21.0012L16.0406 18.0142Z" fill="#34A853"/>
@@ -41,7 +41,7 @@ function LoginForm() {
                             <path d="M5.27698 14.2663C5.03833 13.5547 4.90909 12.7922 4.90909 11.9984C4.90909 11.2167 5.03444 10.4652 5.2662 9.76294L1.23999 6.64844C0.436587 8.25884 0 10.0738 0 11.9984C0 13.918 0.444781 15.7286 1.23746 17.3334L5.27698 14.2663Z" fill="#FBBC05"/>
                         </svg>
                         Sign in with Google
-                    </button>
+                    </button> */}
                 </div>
                 <div className='mt-6 flex justify-center items-center'>
                     <p className='font-medium text-base'>Don't have an account?</p>
