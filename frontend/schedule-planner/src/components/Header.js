43a8d8602146345266c01aafe07895e1f4997f0d
YaraJan <102333657+YaraJan@users.noreply.github.com>
2024-02-09 17:55:11 -0600

    Updated header and made change to Login page

--- frontend/schedule-planner/src/components/Header.js
+++ frontend/schedule-planner/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ToolTip from "@mui/material/Tooltip"
 
 function Header() {
     return (
@@ -11,9 +12,22 @@ function Header() {
             }}
             className='bg-theme-navy grid-rows-4 gap-4'
         >
-            <div className='grid grid-cols-4 gap-4'>
-                <a href="/">Home</a>
-                <a href="/calendar">Calendar</a>
+            <div className='flex flex-row justify-between items-center mr-4 ml-4'>
+                <ToolTip title='Home' arrow='true'>
+                    <a href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                        </svg>
+                    </a>
+                </ToolTip>
+                <ToolTip title='Calendar' arrow='true'>
+                    <a href="/calendar">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
+                        </svg>
+                    </a>
+                </ToolTip>
             </div>
         </div>
     );
