7402fdfe28e8ed8d43b5bb8f01557414f4a14765
reese-hoffart <reese.hoffart@gmail.com>
2024-03-22 15:45:16 -0500

    fixed null strings breaking Course struct

--- backend/src/models/models.go
+++ backend/src/models/models.go
@@ -1,5 +1,7 @@
 package models
 
+import "database/sql"
+
 type Course2 struct {
 	Prefix string `json:"prefix"`
 	Number int `json:"number"`
@@ -12,5 +14,5 @@ type CourseInfo struct {
 	Name string `json:"name"`
 	Level string `json:"level"`
 	Historical_semester string `json:"historical_semester"`
-	Core string `json:"core"`
+	Core sql.NullString `json:"core"`
 }
