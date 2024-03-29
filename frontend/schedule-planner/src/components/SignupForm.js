220bed76e12e1b4393c999e48c62542a35f2d795
YaraJan <102333657+YaraJan@users.noreply.github.com>
2024-03-29 10:51:19 -0500

    Added variables to keep track of input Signup page

--- frontend/schedule-planner/src/components/SignupForm.js
+++ frontend/schedule-planner/src/components/SignupForm.js
@@ -1,6 +1,33 @@
-import * as React from 'react';
+import React, { useState } from 'react'
 
 function SignupForm() {
+    const [netID, setNetID] = useState("")
+    const [fName, setFName] = useState("")
+    const [lName, setLName] = useState("")
+    const [email, setEmail] = useState("")
+    const [pass, setPass] = useState("")
+    const [confirmPass, setConfirmPass] = useState("")
+
+    const idInput = event => {   //handle when input is changing (allows user to type in input)
+        setNetID(event.target.value);
+    }
+    const fNameInput = event => { 
+        setFName(event.target.value);
+    }
+    const lNameInput = event => { 
+        setLName(event.target.value);
+    }
+    const emailInput = event => { 
+        setEmail(event.target.value);
+    }
+    const passInput = event => { 
+        setPass(event.target.value);
+    }
+    const confirmPassInput = event => { 
+        setConfirmPass(event.target.value);
+    }
+    
+    
     return (
         <div className='w-3/4 lg:w-3/4 bg-white px-10 py-5 rounded-3xl border-2 border-blue-800'>
             <h1 className='text-5xl font-semibold'>Sign up</h1>
@@ -11,6 +38,8 @@ function SignupForm() {
                     <input
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Enter your NetID'
+                        value={netID}
+                        onChange={idInput}
                      />
                 </div>
                 <div className='flex flex-row items-center gap-4'>
@@ -18,6 +47,8 @@ function SignupForm() {
                     <input
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Enter your first name'
+                        value={fName}
+                        onChange={fNameInput}
                      />
                 </div>
                 <div className='flex flex-row items-center gap-4'>
@@ -25,6 +56,8 @@ function SignupForm() {
                     <input
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Enter your last name'
+                        value={lName}
+                        onChange={lNameInput}
                      />
                 </div>
                 <div className='flex flex-row items-center gap-4'>
@@ -32,6 +65,8 @@ function SignupForm() {
                     <input
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Enter a valid email address'
+                        value={email}
+                        onChange={emailInput}
                      />
                 </div>
                 <div className='flex flex-col items-center mt-4'>
@@ -41,6 +76,8 @@ function SignupForm() {
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Create your password'
                         type="password"
+                        value={pass}
+                        onChange={passInput}
                      />
                 </div>
                 <div className='flex flex-col items-center mt-2'>
@@ -49,6 +86,8 @@ function SignupForm() {
                         className='w-full border-2 border-blue-100 rounded-xl p-3 mt-1 bg-transparent'
                         placeholder='Retype your password'
                         type="password"
+                        value={confirmPass}
+                        onChange={confirmPassInput}
                      />
                 </div>
                 <div className='mt-6 flex flex-col gap-y-4'>
