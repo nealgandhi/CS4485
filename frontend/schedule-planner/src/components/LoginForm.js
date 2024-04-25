db468d38377b5cadcc74cbf4584ee355fa0c0bc8
nealgandhi <gandhi.neal2002@gmail.com>
2024-04-25 15:58:55 -0500

    Final UI setup

--- frontend/schedule-planner/src/components/LoginForm.js
+++ frontend/schedule-planner/src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
-import LoginButton from "./Login"
 
 function LoginForm() {
     const [email, setEmail] = useState("")
